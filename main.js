ff7b1589bd95af6c199f99b32425ca2b81da71d7
major davis <majordav@msn.com>
2022-01-16 17:38:23 -0700

    added function click me, added onClick, added ffont size on font sheet

--- main.js
+++ main.js
@@ -1,37 +1,24 @@
-//  console.log('Hello World');
+// //  console.log('Hello World');
 
-//  alert('The Man');
+// //  alert('The Man');
 
-// Variables
+// // Variables
 
-let car = 'VW';
-let age = '10 9 8 7 4';
-console.log(age);
+// let car = 'VW';
+// let age = '10 9 8 7 4';
+// console.log(age);
 
-let total = 5 * 3;
-console.log(total);
+// let total = 5 * 3;
+// console.log(total);
 
-// const firstName = document.getElementById('first-name');
- let it = 'Major';
-let firstName = 'My name is ';
-let combine = firstName.concat('Vergil');
-document.getElementById('first-name').innerHTML= 'Good Bye ' + it;
+// // const firstName = document.getElementById('first-name');
+//  let it = 'Major';
+// let firstName = 'My name is ';
+// let combine = firstName.concat('Vergil');
+// document.getElementById('first-name').innerHTML= 'Good Bye ' + it;
+let textColor = document.getElementById('first-name');
 
-function letsClick() {
-    document.getElementById('button').addEventListener('click', 
-    function() {
-      let textColor = document.getElementById('first-name').style.color = 'green';
-     
-     
-     
-    })
-    this.classList.toggle('green');
-// if(textColor ==='green') {
-//     let textColor = document.getElementById('first-name').style.color = 'black'; 
-//     console.log ('my name');
-//     }
-    
-}
-
-console.log ('My name is ', it);
+function letsClick() { 
+    textColor.classList.toggle('green');  
+ }
 
